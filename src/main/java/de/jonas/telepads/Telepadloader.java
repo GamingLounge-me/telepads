60ea29ea27548bf0f2700522964d64e8e310e9bf
jon1games <jonas@saslona.com>
2025-02-18 19:05:58 +0100

    updates dependency

--- src/main/java/de/jonas/telepads/Telepadloader.java
+++ src/main/java/de/jonas/telepads/Telepadloader.java
@@ -11,31 +11,28 @@ import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 
-public class Telepadloader implements PluginLoader{
+public class Telepadloader implements PluginLoader {
 
     @Override
     public void classloader(PluginClasspathBuilder classpathBuilder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
         resolver.addRepository(new RemoteRepository.Builder(
-            "MavenCentralLoader",
-            "default",
-            "https://repo1.maven.org/maven2/"
-        ).build());
+                "MavenCentralLoader",
+                "default",
+                "https://repo1.maven.org/maven2/").build());
         resolver.addDependency(new Dependency(
-            new DefaultArtifact("dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.7.0"),
-            null));
+                new DefaultArtifact("dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.7.0"),
+                null));
         resolver.addDependency(new Dependency(
-            new DefaultArtifact("com.zaxxer:HikariCP:5.1.0"),
-            null));
+                new DefaultArtifact("com.zaxxer:HikariCP:5.1.0"),
+                null));
         resolver.addDependency(new Dependency(
-            new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.3.2"),
-            null, false, Arrays.asList(
-                new Exclusion("com.github.waffle",
-                "waffle-jna",
-                null, null))));
+                new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.3.2"),
+                null, false, Arrays.asList(
+                        new Exclusion("com.github.waffle",
+                                "waffle-jna",
+                                null, null))));
         classpathBuilder.addLibrary(resolver);
     }
 
 }
-
-
