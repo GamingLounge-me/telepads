34cbab3ca981562cc4fdf15eee04a4e4d6fcac41
jon1games <jonas@saslona.com>
2025-02-22 17:35:07 +0100

    add recepi

--- src/main/java/de/jonas/telepads/commands/GiveBuildItem.java
+++ src/main/java/de/jonas/telepads/commands/GiveBuildItem.java
@@ -2,15 +2,13 @@ package de.jonas.telepads.commands;
 
 import org.bukkit.Axis;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.data.Orientable;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import de.jonas.telepads.TelepadCrafting;
 import de.jonas.telepads.Telepads;
 import dev.jorel.commandapi.CommandAPICommand;
-import me.gaminglounge.itembuilder.ItemBuilder;
-import net.kyori.adventure.text.Component;
 
 public class GiveBuildItem {
     Telepads telepads = Telepads.INSTANCE;
@@ -24,10 +22,7 @@ public class GiveBuildItem {
                 .withPermission(conf.getString("GiveBuildItem.permission"))
                 .withAliases(conf.getStringList("GiveBuildItem.aliases").toArray(num -> new String[num]))
                 .executesPlayer((player, arg) -> {
-                    player.getInventory().addItem(new ItemBuilder(Material.BEACON)
-                            .setName(Component.text("Telepad"))
-                            .addBlockPlaceEvent("telepads:buildTelepad")
-                            .build());
+                    player.getInventory().addItem(TelepadCrafting.telepad);
                 })
                 .register();
     }
